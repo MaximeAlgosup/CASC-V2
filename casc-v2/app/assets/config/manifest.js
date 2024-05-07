872ff2ff09f3d451b2e3d190f6b1b87f0b1143df
Maxime CARON <145995231+MaximeAlgosup@users.noreply.github.com>
2024-05-07 15:29:58 +0200

    Add fonts and basic css

--- casc-v2/app/assets/config/manifest.js
+++ casc-v2/app/assets/config/manifest.js
@@ -1,4 +1,7 @@
 //= link_tree ../images
+//= link_tree ../images/logos
+//= link_tree ../images/icons
+//= link_tree ../fonts
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
