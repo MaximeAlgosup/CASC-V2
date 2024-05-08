3c20baa15e1bc5d855165ddf3a8d72fe4e8e663a
Maxime CARON <145995231+MaximeAlgosup@users.noreply.github.com>
2024-05-08 13:30:53 +0200

    merge authentication to navbar

--- casc-v2/config/initializers/i18n.rb
+++ casc-v2/config/initializers/i18n.rb
@@ -4,7 +4,7 @@
 I18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.yml")]
 
 # Locales are supported by our app 
-I18n.available_locales = %i[en fr]
+I18n.available_locales = %i[en]
 
 # Our default locale
 I18n.default_locale = :en
