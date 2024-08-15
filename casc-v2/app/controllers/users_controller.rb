94832f4d316a2d050191f7943b292643f0674976
MaximeAlgosup <maxime.caron@algosup.com>
2024-08-15 20:07:29 +0200

    Start profile page

--- casc-v2/app/controllers/users_controller.rb
+++ casc-v2/app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
     def show 
+        unless current_user.id == params[:id].to_i
+            alert = "You are not authorized to view this page."
+            redirect_to root_path, alert: alert
+        end
         @user = User.find(params[:id])
     end
 end
