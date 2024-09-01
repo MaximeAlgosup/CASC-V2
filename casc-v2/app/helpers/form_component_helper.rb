463592be3ba619dd4ba40d2bc1dda574b308e1e7
MaximeAlgosup <maxime.caron@algosup.com>
2024-09-01 21:47:32 +0200

    Update

--- casc-v2/app/helpers/form_component_helper.rb
+++ casc-v2/app/helpers/form_component_helper.rb
@@ -17,8 +17,57 @@ module FormComponentHelper
         else
             options[:fieldClass] = options[:fieldClass] + " border-none"
         end
+        if value.nil?
+            value = object.send(field)
+        end
         render partial: "partials/form_components/text_field", locals: { form: form, field: field, title: title, object: object, value: value, disabled: disabled, autocomplete: autocomplete, options: options } 
     end
+
+    def passwordField(form, field, title, object, value = nil, disabled = false, autocomplete = "off", custom = false, options = {})
+        if custom
+            if options[:labelClass].nil?
+                options[:labelClass] = ""
+            end
+            if options[:fieldClass].nil?
+                options[:fieldClass] = ""
+            end
+        else
+            options[:labelClass] = "ml-6 green-text text-sm"
+            options[:fieldClass] = "w-full h-full rounded-lg highlight-bg-accent overflow-scroll"
+        end
+        if object.errors[field].any?
+            options[:fieldClass] = options[:fieldClass] + " border border-red-500 text-red-500"
+        else
+            options[:fieldClass] = options[:fieldClass] + " border-none"
+        end
+        if value.nil?
+            value = object.send(field)
+        end
+        render partial: "partials/form_components/password_field", locals: { form: form, field: field, title: title, object: object, value: value, disabled: disabled, autocomplete: autocomplete, options: options }
+    end
+
+    def emailField(form, field, title, object, value = nil, disabled = false, autocomplete = "off", custom = false, options = {})
+        if custom
+            if options[:labelClass].nil?
+                options[:labelClass] = ""
+            end
+            if options[:fieldClass].nil?
+                options[:fieldClass] = ""
+            end
+        else
+            options[:labelClass] = "ml-6 green-text text-sm"
+            options[:fieldClass] = "w-full h-full rounded-lg highlight-bg-accent overflow-scroll"
+        end
+        if object.errors[field].any?
+            options[:fieldClass] = options[:fieldClass] + " border border-red-500 text-red-500"
+        else
+            options[:fieldClass] = options[:fieldClass] + " border-none"
+        end
+        if value.nil?
+            value = object.send(field)
+        end
+        render partial: "partials/form_components/email_field", locals: { form: form, field: field, title: title, object: object, value: value, disabled: disabled, autocomplete: autocomplete, options: options }
+    end      
 
     def textArea(form, field, title, object, value = nil, disabled = false, autocomplete = "off", custom = false, options = {})
         if custom
@@ -36,6 +85,9 @@ module FormComponentHelper
             options[:fieldClass] = options[:fieldClass] + " border border-red-500 text-red-500"
         else
             options[:fieldClass] = options[:fieldClass] + " border-none"
+        end
+        if value.nil?
+            value = object.send(field)
         end
         render partial: "partials/form_components/text_area", locals: { form: form, field: field, title: title, object: object, value: value, disabled: disabled, autocomplete: autocomplete, options: options } 
     end
@@ -56,6 +108,9 @@ module FormComponentHelper
             options[:fieldClass] = options[:fieldClass] + " border border-red-500 text-red-500"
         else
             options[:fieldClass] = options[:fieldClass] + " border-none"
+        end
+        if value.nil?
+            value = object.send(field)
         end
         render partial: "partials/form_components/select_field", locals: { form: form, field: field, title: title, object: object, choices: choices, value: value, disabled: disabled, options: options } 
     end
